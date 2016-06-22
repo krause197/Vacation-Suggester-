62d479e46e704908d449059041830bdd249418ad
Eric Krause <krause197@gmail.com>
2016-06-22 16:34:32 -0700

    Corrections to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,9 @@
 
 $(document).ready(function() {
   $("form#demographics").submit(function(event) {
-    var name = $("input#name").val()
-    var home = $("input#home").val()
-    var age = $ parseInt("input#age").val()
+    var name = $("input#name").val();
+    var home = $("input#home").val();
+    var age = $ parseInt(("input#age").val());
 
 
     event.preventDefault();

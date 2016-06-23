cd6129486f5e2475c1dd6036b373fe2e2fb2fc12
Eric Krause <krause197@gmail.com>
2016-06-23 16:18:41 -0700

    front end user logic complete, start on backend business logic, adjusted .html to match

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,30 @@
-
-
-
-
-
-
+var questionare = function(average) {
+  if (average <=1.75) {
+    return kuwait
+  } else if ((average > 1.75) && (average <= 2.25)) {
+      return baghdad
+  } else if ((average > 2.25) && (average <= 3.25)) {
+      return erbil
+  } else {
+    return afganistan
+  }
+};
 
 $(document).ready(function() {
-  $("form#demographics").submit(function(event) {
+  $("form#questionare").submit(function(event) {
     var name = $("input#name").val();
     var home = $("input#home").val();
-    var age = $ parseInt(("input#age").val());
-
+    var age = parseInt($("input#age").val());
+    var weather = parseInt($("input:radio[name=weather]:checked").val());
+    var scenery = parseInt($("input:radio[name=scenery]:checked").val());
+    var activities = parseInt($("input:radio[name=activities]:checked").val());
+    var average = (weather+scenery+activities)/3;
+    console.log(weather);
+    console.log(scenery);
+    console.log(activities);
+    console.log(average);
 
     event.preventDefault();
 
-
-    alert(result);
   });
 });

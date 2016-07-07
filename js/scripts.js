b9accef04d2bd85163af28b60fa74cb8f12928f1
Eric Krause <krause197@gmail.com>
2016-07-07 10:39:49 -0700

    transfer corrections to javascript and html

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
 var average = function(weather, scenery, activities) {
   return (weather + scenery + activities) / 3;
 };
+var locale;
+
 var questionare = function(average) {
   if (average <=1.75) {
-    var location = "Camp Arifjan, Kuwait";
-    $("#locationResult").show();
+    locale = "Camp Arifjan, Kuwait";
     $("#kuwait-img").show();
   } else if ((average > 1.75) && (average <= 2.25)) {
-    var location = "Baghdad, Iraq";
-    $("#locationResult").show();
+    locale = "Baghdad, Iraq";
     $("#baghdad-img").show();
   } else if ((average > 2.25) && (average <= 3.25)) {
-    var location = "Erbil, Iraq";
-    $("#locationResult").show();
+    locale = "Erbil, Iraq";
     $("#erbil-img").show();
   } else {
-    var location = "Bagram Airfield, Afganistan";
-    $("#locationResult").show();
+    locale = "Bagram Airfield, Afganistan";
     $("#afganistan-img").show();
   }
 };
@@ -32,12 +30,12 @@ $(document).ready(function() {
     var weather = parseInt($("input:radio[name=weather]:checked").val());
     var scenery = parseInt($("input:radio[name=scenery]:checked").val());
     var activities = parseInt($("input:radio[name=activities]:checked").val());
-    console.log(weather);
-    console.log(scenery);
-    console.log(activities);
+    var userAverage = average(weather, scenery, activities);
+    questionare(userAverage);
 
-    $("name").text(name);
-    $("location").text(location);
-    $("home").text(home);
+    $("#locationResult").show();
+    $(".name").text(name);
+    $(".location").text(locale);
+    $(".home").text(home);
   });
 });
